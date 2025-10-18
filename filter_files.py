3150872cbb8ab2e1a4353b06a16cb8f0ca782c11
Rami Abusamra <ramimilkyasir@gmail.com>
2025-10-18 09:39:56 +0100

    adds function to filter items by file extension, extracts copy_files into function, renames check_for_duplicates to better reflect helpers contained in module

---
+++ filter_files.py
@@ -0,0 +1,15 @@
+def delete_duplicates(source_dict, target_dict):
+    files_to_copy = source_dict.copy()
+    for key, value in source_dict.items():
+        if value in target_dict.values():
+            del files_to_copy[key]
+
+    return files_to_copy
+
+
+def filter_by_extension(source_dict, file_extension):
+    if file_extension is not None:
+        files_to_copy = source_dict.copy()
+        for key in source_dict.keys():
+            if key.endswith(file_extension):
+                del files_to_copy[key]
--- main.py
+++ main.py
@@ -2,14 +2,21 @@ import pathlib
 import sys
 import shutil
 from get_hashes_from_directory import hash_files_in_directory
-from check_for_duplicates import delete_duplicates
+from filter_files import delete_duplicates, filter_by_extension
 
 
 def main():
     print(f"starting")
     source_directory = pathlib.Path(sys.argv[1])
     target_directory = pathlib.Path(sys.argv[2])
-    hashed_files_source = hash_files_in_directory(source_directory)
+
+    if sys.argv[3]:
+        file_extension = sys.argv[3]
+    else:
+        file_extension = None
+
+    filtered_source_files = filter_by_extension(source_directory, file_extension)
+    hashed_files_source = hash_files_in_directory(filtered_source_files)
     hashed_files_target = hash_files_in_directory(target_directory)
     files_to_copy = delete_duplicates(hashed_files_source, hashed_files_target)
 
@@ -18,10 +25,7 @@ def main():
 
     proceed_answer = input(f"\nProceed? [y/n]")
     if proceed_answer in ["Y", "y", "yes"]:
-        for item in files_to_copy:
-            target_file_path = f"{target_directory}/{item.name}"
-            shutil.copy(item, target_file_path)
-        print(f"\n{len(files_to_copy)} files copied to {target_directory}")
+        copy_files(target_directory, files_to_copy)
 
     print(f"\nprocess ending")
 
@@ -30,6 +34,13 @@ def print_items(dict):
     for item in dict.keys():
         print(f"Item: {item}, hash: {dict[item]}")
 
+
+def copy_files(target_directory, files_to_copy):
+    for item in files_to_copy:
+        print(f"Copying {item}")
+        target_file_path = f"{target_directory}/{item.name}"
+        shutil.copy(item, target_file_path)
+    print(f"\n{len(files_to_copy)} files copied to {target_directory}")
 
 if __name__ == "__main__":
     main()
